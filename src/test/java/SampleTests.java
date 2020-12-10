f4cd3cf7cd4891d9fa42c8b8741b2fe1f494cca3
Natoloka <natoloka@ukr.net>
2020-12-10 02:31:26 +0200

    Update SampleTests.java
    short fixes

--- src/test/java/SampleTests.java
+++ src/test/java/SampleTests.java
@@ -11,54 +11,56 @@ public class SampleTests {
     @Test
     public void checkHashMap() {
         Map aMap = new HashMap<Integer, String>();
-        aMap.put(0, "Jeronimo");
-        aMap.put(2147483647, "Bombina");
         aMap.put(-2147483647, "Silvio");
-        aMap.put(null, "100500");
-        aMap.put(1, "");
-        aMap.put(-1, null);
-        aMap.put(null, "100501");
-        aMap.put("", "");
-        aMap.put("100600", "");
-        aMap.put("xxx", "6");
-        aMap.put(21474836475l, "Felicita");
-        aMap.put(true, 3);
-
-        Assert.assertEquals(aMap.get(0), "Jeronimo");
-        Assert.assertEquals(aMap.get(2147483647), "Bombina");
+//        expected in HashMap types
         Assert.assertEquals(aMap.get(-2147483647), "Silvio");
+//        key is null
+        aMap.put(null, "100500");
+        Assert.assertEquals(aMap.get(null), "100500");
+//        rewrite value where key is null
+        aMap.put(null, "100501");
         Assert.assertEquals(aMap.get(null), "100501");
+//        value is empty String
+        aMap.put(1, "");
         Assert.assertEquals(aMap.get(1), "");
+//        value is null
+        aMap.put(-1, null);
         Assert.assertTrue(aMap.get(-1)==null);
+//        key is empty String
+        aMap.put("", "");
         Assert.assertTrue(aMap.get("").equals(""));
-        Assert.assertTrue(aMap.get("100600").equals(""));
-        Assert.assertTrue(aMap.get("xxx").equals("6"));
+//        key is String
+        aMap.put("xxx", "6");
+//        key is Long
+        aMap.put(21474836475l, "Felicita");
         Assert.assertTrue(aMap.get(21474836475l).equals("Felicita"));
+//        key is boolean, value int
+        aMap.put(true, 3);
         Assert.assertTrue(aMap.get(true).equals(3));
+//        try with non-existed boolean key
         Assert.assertNull(aMap.get(false));
-        Assert.assertNull(aMap.get(54321));
     }
 
     @Test
     public void checkSortedMap() throws NullPointerException{
         SortedMap sMap = new TreeMap<Integer, String>();;
         sMap.put(0, "Jeronimo");
+        Assert.assertEquals(sMap.get(0), "Jeronimo");
         sMap.put(2147483647, "Bombina");
+        Assert.assertEquals(sMap.get(2147483647), "Bombina");
         sMap.put(-2147483647, "Silvio");
+        Assert.assertEquals(sMap.get(-2147483647), "Silvio");
         sMap.put(1, "");
+        Assert.assertEquals(sMap.get(1), "");
         sMap.put(-1, null);
+        Assert.assertNull(sMap.get(-1));
+
         Assert.assertThrows(NullPointerException.class, ()->{sMap.put(null, "100500");});
         Assert.assertThrows(ClassCastException.class, ()->{sMap.put("", "");});
         Assert.assertThrows(ClassCastException.class, ()->{sMap.put("gg", "rf");});
         Assert.assertThrows(ClassCastException.class, ()->{sMap.put(21474836475l, "rf");});
         Assert.assertThrows(ClassCastException.class, ()->{sMap.put(true, "rf");});
 
-        Assert.assertEquals(sMap.get(0), "Jeronimo");
-        Assert.assertEquals(sMap.get(2147483647), "Bombina");
-        Assert.assertEquals(sMap.get(-2147483647), "Silvio");
-        Assert.assertEquals(sMap.get(1), "");
-        Assert.assertNull(sMap.get(-1));
     }
 
 }
-
